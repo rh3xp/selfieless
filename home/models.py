77ac875d44552f993ffa7499a3c7fe0986572f8c
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-12 19:47:43 +0530

    created models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class profile(models.Model):
+    uname = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    passwd = models.CharField(max_length=20)
+    profile_pic = models.CharField(max_length=1000)
