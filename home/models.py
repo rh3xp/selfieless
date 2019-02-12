30eda35926013cb8f5302554540d9534b5c95895
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-12 20:59:21 +0530

    added datetimefield in home/models.py

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class profile(models.Model):
     email = models.CharField(max_length=50)
     passwd = models.CharField(max_length=20)
     profile_pic = models.CharField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.uname + '-' + self.email
