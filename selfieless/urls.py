5f538a5ced989bb3c3fab4b267c6dc6cacf0b94c
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-12 19:29:44 +0530

    removed $ and re_path -> empty string

--- selfieless/urls.py
+++ selfieless/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$', include('home.urls')),
+    path('', include('home.urls')),
     path('home/', include('home.urls')),
 ]
