67f728a6ba42a14348c65eba2f5ab92a8be55b9e
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-12 21:08:07 +0530

    type casted datetime -> str

--- home/models.py
+++ home/models.py
@@ -6,11 +6,11 @@ class profile(models.Model):
     uname = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     passwd = models.CharField(max_length=20)
-    profile_pic = models.CharField(max_length=1000)
+    profile_pic = models.CharField(max_length=1000, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.uname + '-' + self.email
+        return self.uname + '-' + str(self.created)
 
 class posts(models.Model):
     posts_uname = models.ForeignKey(profile, on_delete=models.CASCADE)
