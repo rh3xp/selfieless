da87019ce7160e88ddbe9a7da47a3a7f1f9302b5
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-12 21:18:04 +0530

    created profile -> home/views

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         re_path(r'^$', views.index, name='index'),
+        re_path(r'^(?P<user_id>[0-9]+)/$', views.profile, name='profile' ),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>this is the index fx in home</h1>")
+
+def profile(request, user_id):
+    return HttpResponse("<h2>User id: " + str(user_id) + "</h2>")
