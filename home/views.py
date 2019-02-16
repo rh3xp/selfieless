045abc270bc363760b3fcb573a2b7b4a40bba22c
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-17 01:39:02 +0530

    Updated home.views with new details page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 from .models import Users
 
 
@@ -8,19 +7,12 @@ from .models import Users
 
 def index(request):
     all_users = Users.objects.all()
-    template = loader.get_template('home/index.html')
-
-    context = {
-            'all_users' : all_users,
-        }
-    '''
-    html = ''
-    for user in all_users:
-        url = '/home/' + str(user.id) + '/'
-        html += '<a href = "' + url + '">' + user.uname + '</a><br>'
-    '''
-    return HttpResponse(template.render(context, request))
-    #return HttpResponse("<h1>this is the index fx in home</h1>")
+    return render(request, 'home/index.html', {'all_users' : all_users})
 
 def detail(request, user_id):
-    return HttpResponse("<h2>User id: " + str(user_id) + "</h2>")
+    try:
+        user = Users.objects.get(pk=user_id)
+    except Users.DoesNotExist:
+        raise Http404("User not found")
+
+    return render(request, 'home/detail.html', {'user' : user})
