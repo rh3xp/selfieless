715cba31a57fbb0884bbb3123ad02961c6a44a86
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-17 08:57:38 +0530

    fixed ImportError -> home/urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.conf.urls import url
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
