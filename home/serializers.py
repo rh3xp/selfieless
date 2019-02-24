6b87b9ee9052bf0fbde9733c2a39e10db9176156
Parth Krishna <thenerdsuperuser@gmail.com>
2019-02-24 16:45:41 +0530

    deleted code. added requirements

--- home/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Users
-
-class UsersSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Users
-		fields = ('id', 'uname', 'email', 'passwd', 'profile_pic', 'created')
-		read_only_fields = ('profile_pic', 'created')
-			
