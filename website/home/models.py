20744145c45b8dbb8cf4300c6e2e5a2cecaf2036
Parth Krishna <thenerdsuperuser@gmail.com>
2019-02-24 18:45:34 +0530

    created models in home->models

--- website/home/models.py
+++ website/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    uname = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    passwd = models.CharField(max_length=20)
+    profile_pic = models.CharField(max_length=1000, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.uname + '-' + str(self.created)
+
+class Post(models.Model):
+    posts_uname = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.CharField(max_length=100)
