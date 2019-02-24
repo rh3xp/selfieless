2ac8a57c70da4278b0a142ad5bc1fe63043a294a
Parth Krishna <thenerdsuperuser@gmail.com>
2019-02-24 18:26:19 +0530

    added apps to website->settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'home.apps.HomeConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
