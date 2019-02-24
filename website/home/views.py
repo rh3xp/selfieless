b4bd722db5acdff219db6b02c4c483bd6f800666
Parth Krishna <thenerdsuperuser@gmail.com>
2019-02-24 18:42:41 +0530

    Rendering data using home->views

--- website/home/views.py
+++ website/home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+
+
+def index(request):
+    all_users = Users.objects.all()
+    return render(request, 'home/index.html', {'all_users' : all_users})
+
+def detail(request, user_id):
+    try:
+        user = Users.objects.get(pk=user_id)
+    except Users.DoesNotExist:
+        raise Http404("User not found")
+
+    return render(request, 'home/detail.html', {'user' : user})
+
