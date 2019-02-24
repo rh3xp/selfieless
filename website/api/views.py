86231fd33b981f0ecbadc9253a675aa6630fada9
Parth Krishna <thenerdsuperuser@gmail.com>
2019-02-24 18:59:51 +0530

    added views api->views

--- website/api/views.py
+++ website/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from home.models import User
+
 
 # Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
